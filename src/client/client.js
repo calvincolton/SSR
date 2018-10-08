c5d2255030f27bcb03b921afe3a6c1aa2c0f6b5d
Calvin Colton <calvincolton@gmail.com>
2018-10-08 09:31:05 -0700

    AND importing reducers into client index, AKA client.js, and createStore server helper

--- src/client/client.js
+++ src/client/client.js
@@ -6,6 +6,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import Routes from './Routes';
+import reducers from './reducers';
 
 const store = createStore(reducers, {}, applyMiddleware(thunk));
 
