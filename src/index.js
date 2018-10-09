4574c3738b8858751271798f62f27e0b270590c0
Calvin Colton <calvincolton@gmail.com>
2018-10-08 19:38:44 -0700

    UsersList component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill';
 import express from 'express';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
@@ -11,7 +12,7 @@ app.get('*', (req, res) => {
 
   // Logic to initialize
   // and load data into the store
-  
+
   res.send(renderer(req, store));
 });
 
